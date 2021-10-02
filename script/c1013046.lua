72fb0e3c710f63cafc28fc5d40454b4957addcc0
KServantes <elitalianoverde@gmail.com>
2021-10-02 08:31:35 -0600

    tweak to Shadow Pendulum Specter

--- script/c1013046.lua
+++ script/c1013046.lua
@@ -1,8 +1,4 @@
 --Shadow Pendulum Specter
---[[You can destroy this card in your Pendulum Zone, and if you do, this card gains this effect while face-up in the Extra Deck:
-● If you have only 1 card in your Pendulum Zone: You can select 1 face-up Pendulum card in your Extra Deck or field; place it in your Pendulum Zone.
-● If you have 2 cards in your Pendulum Zones: You can destroy both cards, and if you do, draw 1 card.
-You can only activate each effect of "Shadow Pendulum Specter" once per turn.]]--
 local cod,id = GetID()
 function cod.initial_effect(c)
 	--Pendulum Set
@@ -27,42 +23,44 @@ end
 function cod.teop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if not c:IsRelateToEffect(e) then return end
-	if Duel.Destroy(c)>0 then
+	if Duel.Destroy(c, REASON_EFFECT)>0 then
 		--Place in Pzone
 		local e2=Effect.CreateEffect(c)
 		e2:SetType(EFFECT_TYPE_IGNITION)
 		e2:SetRange(LOCATION_EXTRA)
-		e2:SetLabel(1)
-		e2:SetCountLimit(1,id)
+		e2:SetLabel(100)
 		e2:SetCondition(cod.plcon)
 		e2:SetTarget(cod.pltg)
 		e2:SetOperation(cod.plop)
-		e2:SetReset(RESETS_STANDARD)
+		e2:SetReset(RESET_EVENT+0x1bf0000)
 		c:RegisterEffect(e2)
 		--Draw
 		local e3=e2:Clone()
 		e3:SetCategory(CATEGORY_DRAW+CATEGORY_DESTROY)
-		e3:SetLabel(2)
+		e3:SetLabel(200)
 		e3:SetTarget(cod.drtg)
 		e3:SetOperation(cod.drop)
 		c:RegisterEffect(e3)
+	end
 end
 
 --Common Condition
 function cod.plcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetMatchingGroupCount(Card.IsFaceup, LOCATION_PZONE, 0, nil)==e:GetLabel()
+	return Duel.GetMatchingGroupCount(Card.IsFaceup, tp, LOCATION_PZONE, 0, nil)==(e:GetLabel()/100) and Duel.GetFlagEffect(tp, id+e:GetLabel())==0
 end
 
 --Place in Pzone
+function cod.filter(c)
+	return c:IsType(TYPE_PENDULUM) and c:IsFaceup() and not c:IsLocation(LOCATION_PZONE)
+end
 function cod.pltg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local filter = aux.AND(aux.FilterBoolFunction(Card.IsType, TYPE_PENDULUM), Card.IsFaceup)
-	if chk==0 then return Duel.IsExistingMatchingCard(filter, LOCATION_EXTRA+LOCATION_FIELD, 0, 1, nil) end
+	if chk==0 then return Duel.IsExistingMatchingCard(cod.filter, tp, LOCATION_EXTRA+LOCATION_ONFIELD, 0, 1, e:GetHandler()) end
+	Duel.RegisterFlagEffect(tp, id+e:GetLabel(), RESET_PHASE+PHASE_END, 0, 0)
 end
 function cod.plop(e,tp,eg,ep,ev,re,r,rp)
-	local filter = aux.AND(aux.FilterBoolFunction(Card.IsType, TYPE_PENDULUM), Card.IsFaceup)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOZONE)
-	local g=Duel.SelectMatchingCard(tp, filter, tp, LOCATION_EXTRA+LOCATION_FIELD, 0, 1, 1, nil)
-	if #g<0 or not (Duel.CheckLocation(tp, LOCATION_PZONE, 0) or Duel.CheckLocation(tp, LOCATION_PZONE, 1)) return end
+	local g=Duel.SelectMatchingCard(tp, cod.filter, tp, LOCATION_EXTRA+LOCATION_ONFIELD, 0, 1, 1, e:GetHandler())
+	if #g<0 or not (Duel.CheckLocation(tp, LOCATION_PZONE, 0) or Duel.CheckLocation(tp, LOCATION_PZONE, 1)) then return end
 	Duel.MoveToField(g:GetFirst(), tp, tp, LOCATION_PZONE, POS_FACEUP, true)
 end
 
@@ -76,6 +74,7 @@ function cod.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
     Duel.SetTargetPlayer(tp)
     Duel.SetTargetParam(1)
     Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+    Duel.RegisterFlagEffect(tp, id+e:GetLabel(), RESET_PHASE+PHASE_END, 0, 0)
 end
 function cod.drop(e,tp,eg,ep,ev,re,r,rp)
     local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
