38ea7ed3427c500ceb6bc5dc285b1274c055b121
KServantes <elitalianoverde@gmail.com>
2021-10-03 07:39:56 -0600

    tweaks to bone mouse

--- script/c1013047.lua
+++ script/c1013047.lua
@@ -27,39 +27,40 @@ function c1013047.initial_effect(c)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetCategory(CATEGORY_TODECK)
 	e2:SetRange(LOCATION_PZONE)
+	e2:SetCost(cod.drcost)
 	e2:SetTarget(cod.drtg)
 	e2:SetOperation(cod.drop)
 	c:RegisterEffect(e2)
 end
 
+--Recover
 function cod.sumfilter(c)
-	return c:GetType()==TYPE_FUSION+TYPE_PENDULUM and c:GetSummonType()==SUMMON_TYPE_FUSION
-		and c:GetMateriaCount() == c:GetMaterial():FilterCount(aux.AND(Card.IsFaceup, aux.FilterBoolFunction(Card.IsLocation,LOCATION_EXTRA)),nil)
+	-- Debug.Message()
+	if not c:IsType(TYPE_FUSION) then return false end
+	return c:IsType(TYPE_PENDULUM) and c:GetSummonType()==SUMMON_TYPE_FUSION
+		and c:GetMaterialCount() == c:GetMaterial():FilterCount(aux.AND(Card.IsFaceup, aux.FilterBoolFunction(Card.IsLocation,LOCATION_EXTRA)),nil)
 end
 function cod.rccon(e,tp,eg,ep,ev,re,r,rp)
-	if eg:IsExists(cod.sumfilter,1,nil) then
-		if #eg==1 then
-			e:SetLabel(eg:GetFirst():GetMaterialCount())
-			e:SetLabelObject(eg:GetFirst():GetMaterial())
-		else
-			e:SetLabel(99)
-			e:SetLabelObject(eg)
-		end
-	end
+	return eg:IsExists(cod.sumfilter,1,nil) 
 end
 function cod.rctg(e,tp,eg,ep,ev,re,r,rp,chk)
+	Debug.Message(Duel.IsExistingMatchingCard(cod.sumfilter,tp,LOCATION_EXTRA,0,1,nil))
 	if chk==0 then return e:GetHandler():IsDestructable() 
-		and Duel.IsExistingMatchingCard(cod.sumfilter,tp,LOCATION_EXTRA,0,1,nil) end
+		and Duel.IsExistingMatchingCard(cod.sumfilter,tp,LOCATION_MZONE,0,1,nil) end
 	Duel.Destroy(e:GetHandler(), REASON_EFFECT)
-	local g
-	if e:GetHandler()~=99 then
-		g = e:GetLabelObject()
+	local g=Group.CreateGroup()
+	if #eg==1 then
+		Debug.Message(#eg)
+		Debug.Message(eg:GetFirst():GetMaterialCount())
+		g=eg:GetFirst():GetMaterial()
 	else
 		Duel.Hint(HINT_SELECTMSG,tp, HINTMSG_ATOHAND)
-		local sc=e:GetLabelObject():FilterSelect(cod.sumfilter,1,1,nil)
-		g = sc:GetMaterial()
-		e:SetLabelObject(g)
+		local sc=eg:FilterSelect(cod.sumfilter,1,1,nil)
+		g=sc:GetMaterial()
 	end
+	if g==0 then return end
+	e:SetLabelObject(g)
+	g:KeepAlive()
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,#g,tp,LOCATION_EXTRA)
 end
 function cod.rcop(e,tp,eg,ep,ev,re,r,rp)
@@ -70,26 +71,30 @@ function cod.rcop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.SendtoHand(g,nil,REASON_EFFECT)
 end
 
+--Draw
 function cod.helper(tp)
-	local i = 1
-	table = [1, 2, 3]
-	draw = 0
-	for i in table do
-		if not Duel.IsPlayerCanDraw(tp, i) then
+	local draw = 0
+	for i=1, 3 do
+		if Duel.IsPlayerCanDraw(tp, i) then
 			draw = i
 		end
 	end
+	return draw
 end
-function cod.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsPlayerCanDraw(tp, 1) 
-		and Duel.IsExistingMatchingCard(Card.IsFaceup,tp,LOCATION_EXTRA,0,2,nil) end
+function cod.drcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsFaceup,tp,LOCATION_EXTRA,0,2,nil) end
 	local ct = cod.helper(tp)
 	local max = ct*2
+	if max<2 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
 	local g=Duel.SelectMatchingCard(tp,Card.IsFaceup,tp,LOCATION_EXTRA,0,2,max,nil)
-	Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
+	local sc=Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
+	e:SetLabel(sc)
+end
+function cod.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetLabel() and Duel.IsPlayerCanDraw(tp, e:GetLabel()/2) end
 	Duel.SetTargetPlayer(tp)
-    Duel.SetTargetParam(1)
+    Duel.SetTargetParam(e:GetLabel())
     Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
 end
 function cod.drop(e,tp,eg,ep,ev,re,r,rp)
