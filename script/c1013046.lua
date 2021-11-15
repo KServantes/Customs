101242ee944b6fa470bd3d59364794a686706ca6
KServantes <elitalianoverde@gmail.com>
2021-11-15 16:46:10 -0700

    new effect

--- script/c1013046.lua
+++ script/c1013046.lua
@@ -3,61 +3,76 @@ local cod,id = GetID()
 function cod.initial_effect(c)
 	--Pendulum Set
 	Pendulum.AddProcedure(c)
-	--Gain Effect
+
+	---[Pendulum Effects]
+	--Special Summon
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetType(EFFECT_TYPE_QUICK_O)
-	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_PZONE)
-	e1:SetCountLimit(1,id)
-	e1:SetTarget(cod.tetg)
-	e1:SetOperation(cod.teop)
+	e1:SetCountLimit(1,id+100)
+	e1:SetTarget(cod.sptg)
+	e1:SetOperation(cod.spop)
 	c:RegisterEffect(e1)
+
+	---[Monster Effects]
+	--Place
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_HAND)
+	e2:SetCost(cod.plcost)
+	e2:SetTarget(cod.pltg)
+	e2:SetOperation(cod.plop)
+	c:RegisterEffect(e2)
+	--Special Summon
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,2))
+	e3:SetCategory(CATEGORY_TODECK+CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_GRAVE)
+	e3:SetTarget(cod.sptg2)
+	e3:SetOperation(cod.spop2)
+	c:RegisterEffect(e3)
 end
 
---Gain Effect
-function cod.tetg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():IsDestructable() end
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,e:GetHandler(),1,0,0)
+
+--Special in Pzone
+function cod.sfilter(c,e,tp)
+	return	c:IsType(TYPE_PENDULUM) and not c:IsType(TYPE_EFFECT) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function cod.teop(e,tp,eg,ep,ev,re,r,rp)
+function cod.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	if not c:IsRelateToEffect(e) then return end
-	if Duel.Destroy(c, REASON_EFFECT)>0 then
-		--Place in Pzone
-		local e2=Effect.CreateEffect(c)
-		e2:SetDescription(aux.Stringid(id,1))
-		e2:SetType(EFFECT_TYPE_IGNITION)
-		e2:SetRange(LOCATION_EXTRA)
-		e2:SetLabel(100)
-		e2:SetCondition(cod.plcon)
-		e2:SetTarget(cod.pltg)
-		e2:SetOperation(cod.plop)
-		e2:SetReset(RESET_EVENT+0x1bf0000)
-		c:RegisterEffect(e2)
-		--Draw
-		local e3=e2:Clone()
-		e3:SetDescription(aux.Stringid(id,2))
-		e3:SetCategory(CATEGORY_DRAW)
-		e3:SetLabel(200)
-		e3:SetTarget(cod.drtg)
-		e3:SetOperation(cod.drop)
-		c:RegisterEffect(e3)
-	end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and c:IsDestructable()
+		and Duel.IsExistingMatchingCard(cod.sfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
-
---Common Condition
-function cod.plcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetMatchingGroupCount(Card.IsFaceup, tp, LOCATION_PZONE, 0, nil)==(e:GetLabel()/100) and Duel.GetFlagEffect(tp, id+e:GetLabel())==0
+function cod.spop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	if Duel.Destroy(e:GetHandler(),REASON_EFFECT)==0 then return end
+	local lc=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if lc<=0 then return end
+	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then lc=1 end
+	if lc>2 then lc==3 end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,cod.sfilter,tp,LOCATION_DECK,0,1,lc,nil)
+	if #g<=0 then return end
+	Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 end
 
 --Place in Pzone
+function cod.plcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsDiscardable() end
+	Duel.SendtoGrave(e:GetHandler(),REASON_COST+REASON_DISCARD)
+end
 function cod.filter(c)
 	return c:IsType(TYPE_PENDULUM) and c:IsFaceup() and not c:IsLocation(LOCATION_PZONE)
 end
 function cod.pltg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(cod.filter, tp, LOCATION_EXTRA+LOCATION_ONFIELD, 0, 1, e:GetHandler()) end
-	Duel.RegisterFlagEffect(tp, id+e:GetLabel(), RESET_PHASE+PHASE_END, 0, 0)
 end
 function cod.plop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOZONE)
@@ -66,19 +81,19 @@ function cod.plop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.MoveToField(g:GetFirst(), tp, tp, LOCATION_PZONE, POS_FACEUP, true)
 end
 
---Draw
-function cod.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsFaceup,tp,LOCATION_PZONE,0,2,nil) and Duel.IsPlayerCanDraw(tp,1) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectMatchingCard(tp,Card.IsFaceup,tp,LOCATION_PZONE,0,2,2,nil)
-	if #g<2 then return end
-	Duel.Destroy(g,REASON_EFFECT+REASON_COST)
-    Duel.SetTargetPlayer(tp)
-    Duel.SetTargetParam(1)
-    Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
-    Duel.RegisterFlagEffect(tp, id+e:GetLabel(), RESET_PHASE+PHASE_END, 0, 0)
+--Special Summon from Pzone
+function cod.sptg2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToDeck()
+		and Duel.IsExistingMatchingCard(cod.pfilter,tp,LOCATION_PZONE,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,e:GetHandler(),1,tp,LOCATION_DECK)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_PZONE)
 end
-function cod.drop(e,tp,eg,ep,ev,re,r,rp)
-    local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
-    Duel.Draw(p,d,REASON_EFFECT)
+function cod.spop2(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	if Duel.SendtoDeck(c,nil,REASON_EFFECT)==0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,cod.pfilter,tp,LOCATION_PZONE,0,1,1,nil,e,tp)
+	if #g<=0 then return end
+	Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 end
